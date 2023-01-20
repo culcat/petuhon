8f6d1c4b31ee09cf3ed9f4ea08aa444e35884810
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2023-01-20 20:42:19 +0300

    5 complete

---
+++ 5zd/main.py
@@ -0,0 +1,7 @@
+from random import randint
+randV = randint(0,4)
+randF = randint(0,4)
+vkusi = ['vkus1','vkus2','vkus3','vkus4','vkus5']
+form  = ['form1','form2','form3','form4','form5']
+
+print("мармеладка:\nвкус:",vkusi[randV],'\nформа:',form[randF] )
