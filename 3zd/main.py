a444273e586201262695f8194ce9fe3b802bea04
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2023-01-21 15:45:23 +0300

    сделал 3-е

--- 3zd/main.py
+++ 3zd/main.py
@@ -1,8 +1,11 @@
-a = int(input())
-m = a%10
-a = a//10
-while a > 0:
-    if a%10 > m:
-        m = a%10
-    a = a//10
-print(m,m)
+
+a=['1','2','5','4','1']
+m = max(a)
+i = -1
+while i< len(a):
+    i=i+1
+    if a[i] == m:
+        a[i]=a[i]*2
+        print(a)
+
+
