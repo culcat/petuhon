5b15c974ca6ca50cce17fb74cce12cd7c32b3617
Alexandr Krainukov <krainukovaleksandr1@gmail.com>
2023-01-21 14:07:12 +0300

    4 задание готово

---
+++ 4zd/main.py
@@ -0,0 +1,14 @@
+a = int(input('Загадайте число:'))
+b = int(input('Загадайте число:'))
+q = 0
+if b<q:
+    print("нет")
+elif a<q:
+    print('нет')
+else:
+    a = a ** 2
+    b =b ** 2
+    c = a+b
+    print(c)  
+
+  
